879b2e3b4b555472e5c0cc5332b9235c14664eca
bom_d_van <bom.d.van@gmail.com>
2015-10-19 22:28:28 +0800

    add basic implementation

---
+++ sidekick.go
@@ -0,0 +1,54 @@
+package sidekick
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+type stringSlice []string
+
+func (ss stringSlice) String() string      { return strings.Join(ss, ",") }
+func (ss *stringSlice) Set(s string) error { *ss = append(*ss, s); return nil }
+
+var (
+	skips stringSlice
+	cases stringSlice
+
+	// Debug is similar to testing.Verbose. Set to true when using flag
+	// -debug.
+	Debug bool
+)
+
+func init() {
+	flag.Var(&skips, "skip", "skip test case")
+	flag.Var(&cases, "case", "run test case")
+	flag.BoolVar(&Debug, "debug", false, "enter debug mode")
+}
+
+// SkipCase checks if a test case should be run based on flags -skip or -case.
+// SkipCase compares values in string form, you can pass in value has
+// fmt.Stringer or any other interfaces that package fmt has supported.
+func SkipCase(c interface{}) bool {
+	for _, item := range skips {
+		if item == fmt.Sprint(c) {
+			return true
+		}
+	}
+
+	for _, item := range cases {
+		if item == fmt.Sprint(c) {
+			return false
+		}
+	}
+
+	if len(cases) > 0 {
+		return true
+	}
+
+	return false
+}
+
+// TODO
+// type Skiper interface{}
+// type Caser interface{}
---
+++ sidekick_test.go
@@ -0,0 +1,71 @@
+package sidekick
+
+import (
+	"flag"
+	"log"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+func TestSkipCase(t *testing.T) {
+	oldArgs := os.Args
+	for _, c := range []struct {
+		input        []string
+		intResult    map[int]bool
+		stringResult map[string]bool
+	}{
+		{
+			input:        []string{"-skip", "1", "-skip", "2"},
+			intResult:    map[int]bool{0: true, 3: true},
+			stringResult: map[string]bool{"0": true, "3": true},
+		},
+		{
+			input:        []string{"-case", "1", "-case", "2"},
+			intResult:    map[int]bool{1: true, 2: true},
+			stringResult: map[string]bool{"1": true, "2": true},
+		},
+	} {
+		os.Args = append(oldArgs, c.input...)
+		skips = []string{}
+		cases = []string{}
+		flag.Parse()
+
+		intResult := map[int]bool{}
+		for i := range []int{0, 1, 2, 3} {
+			if SkipCase(i) {
+				continue
+			}
+			intResult[i] = true
+		}
+		if got, want := intResult, c.intResult; !reflect.DeepEqual(got, want) {
+			t.Errorf("intResult = %#v; want %#v", got, want)
+		}
+
+		stringResult := map[string]bool{}
+		for _, i := range []string{"0", "1", "2", "3"} {
+			if SkipCase(i) {
+				continue
+			}
+			stringResult[i] = true
+		}
+		if got, want := stringResult, c.stringResult; !reflect.DeepEqual(got, want) {
+			t.Errorf("stringResult = %#v; want %#v", got, want)
+		}
+	}
+}
+
+func ExampleSkipCase() {
+	// skip case or cases: go test -skip 1 -skip 2
+	for i, c := range cases {
+		if SkipCase(i) {
+			continue
+		}
+		// you can filter cases not needed when debugging tests
+		_ = c
+	}
+}
